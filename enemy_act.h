3a32a58d31afb39e0af790eb6e6fd4e5564e9ccf
LAPTOP-RLA0U253\NATSUKI <natsunatsuki.06.24@gmail.com>
2017-07-20 13:49:45 +0900

    enemy_act.h更新

--- enemy_act.h
+++ enemy_act.h
@@ -1,14 +1,15 @@
 #ifndef ENEMY_ACT_H
 #define ENEMY_ACT_H
 
-void enemy_act0(int);
-void enemy_act1(int);
-void enemy_act2(int);
-void enemy_act3(int);
-void enemy_act4(int);
-void enemy_act5(int);
-void enemy_act6(int);
-void enemy_act7(int);
-void enemy_act8(int);
 
-#endif 
+void enemy_act0(Enemy_status *enemy,float deg);
+void enemy_act1(void);
+void enemy_act2(void);
+void enemy_act3(Enemy_status *enemy);
+void enemy_act4(Enemy_status *enemy);
+void enemy_act5(Enemy_status *enemy);
+void enemy_act6(Enemy_status *enemy);
+void enemy_act7(Enemy_status *enemy, float x1, float y1, float x2, float y2, float x3, float y3);
+void enemy_act8(Enemy_status *enemy, float x1, float y1, float x2, float y2);
+
+#endif
